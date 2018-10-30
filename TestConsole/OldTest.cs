eb7302c8fe8b5793bda5ca399e5480ec40bc5b1e
Jason Yu <jason_yu@executivecentre.com>
2018-10-30 17:53:49 +0800

    Try out custom JsonConverter

--- TestConsole/OldTest.cs
+++ TestConsole/OldTest.cs
@@ -15,6 +15,37 @@ namespace TestConsole
 {
     internal class OldTest
     {
+        public static void TestTimeRange2()
+        {
+            var timeRange = new TimeRange(new DateTime(2018, 11, 22, 10, 15, 00), Duration.Minutes(30));
+
+            timeRange.ExpandStartTo(new DateTime(2018, 11, 22, 10, 14, 00));
+
+            Console.WriteLine($"Result: {timeRange}");
+        }
+
+        public static void TestTimeGap()
+        {
+            var schedule = new TimeBlock(new DateTime(2018, 10, 10, 08, 30, 00), Duration.Minutes(180));
+            var bookings = new TimePeriodCollection
+            {
+                new TimeRange(new DateTime(2018, 10, 10, 08, 00, 0), Duration.Minutes(60)),
+                new TimeRange(new DateTime(2018, 10, 10, 09, 30, 0), Duration.Minutes(30)),
+                new TimeRange(new DateTime(2018, 10, 10, 10, 00, 0), Duration.Minutes(30)),
+            };
+            var selected = new TimeBlock(new DateTime(2018, 10, 10, 09, 30, 0), Duration.Minutes(30));
+            Console.WriteLine($"Selected: {selected}");
+            Console.WriteLine();
+
+            // calculate the gaps using the time calendar as period mapper
+            var availableTimes = new TimeGapCalculator<TimeBlock>()
+                .GetGaps(bookings, schedule)
+                .Where(t => t.Duration >= selected.Duration)
+                .SelectMany(t => t.Split(selected.Duration));
+            foreach (var t in availableTimes)
+                Console.WriteLine($"Result: {t}");
+        }
+
         public static void TestNamespace()
         {
             var test = new ChildClass();
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -2,45 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
+using TestConsole.TestJson;
 
 namespace TestConsole
 {
     internal class Program
     {
-        public static void TestTimeGap()
+        public static void TestJson()
         {
-            var schedule = new TimeBlock(new DateTime(2018, 10, 10, 08, 30, 00), Duration.Minutes(180));
-            var bookings = new TimePeriodCollection
-            {
-                new TimeRange(new DateTime(2018, 10, 10, 08, 00, 0), Duration.Minutes(60)),
-                new TimeRange(new DateTime(2018, 10, 10, 09, 30, 0), Duration.Minutes(30)),
-                new TimeRange(new DateTime(2018, 10, 10, 10, 00, 0), Duration.Minutes(30)),
-            };
-            var selected = new TimeBlock(new DateTime(2018, 10, 10, 09, 30, 0), Duration.Minutes(30));
-            Console.WriteLine($"Selected: {selected}");
-            Console.WriteLine();
+            var test = JsonConvert.SerializeObject(new JsonObject {NeedToAddPrefix = 123});
 
-            // calculate the gaps using the time calendar as period mapper
-            var availableTimes = new TimeGapCalculator<TimeBlock>()
-                .GetGaps(bookings, schedule)
-                .Where(t => t.Duration >= selected.Duration)
-                .SelectMany(t => t.Split(selected.Duration));
-            foreach (var t in availableTimes)
-                Console.WriteLine($"Result: {t}");
-        }
+            var result = JsonConvert.DeserializeObject<JsonObject>(test);
 
-        public static void TestTimeRange()
-        {
-            var timeRange = new TimeRange(new DateTime(2018, 11, 22, 10, 15, 00), Duration.Minutes(30));
-
-            timeRange.ExpandStartTo(new DateTime(2018, 11, 22, 10, 14, 00));
-
-            Console.WriteLine($"Result: {timeRange}");
+            Console.WriteLine(test);
         }
 
         private static void Main(string[] args)
         {
-            TestTimeRange();
+            TestJson();
             Console.ReadKey();
         }
     }
